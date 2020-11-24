d6aecee758fe59240af009307f6689e2d824e4c4
HeatherAckerman <hla20115213@gmail.com>
2020-11-24 15:22:57 -0600

    react pages are routed and being rendered

---
+++ src/components/Container.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Nav from "./Nav";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+
+class Container extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === "Home") {
+      return <Home />;
+    } else if (this.state.currentPage === "Portfolio") {
+      return <Portfolio />
+    } else {
+      return <Contact />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Nav
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default Container;
