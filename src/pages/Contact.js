64a4b70de9c2b5d586a2ee1fef101628accef4d8
HeatherAckerman <hla20115213@gmail.com>
2020-11-29 14:04:44 -0600

    added resume to contact page and changed some styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import Resume from '../assets/Resume.pdf';
 
 const Contact = () => (
   <div>
@@ -9,22 +10,17 @@ const Contact = () => (
           <h1>Contact Me</h1><hr></hr>
         </Col>
       </Row>
+      <br />
       <Row>
-        <h5>Find me on:</h5>
+        <Button href={Resume} target="blank" className="contactBtn">Resume</Button>
       </Row>
+      <br />
       <Row>
         <Button href="https://www.linkedin.com/in/heather-ackerman-a6a869b1/" target="blank" className="contactBtn">LinkedIn</Button>
       </Row>
       <br />
       <Row>
         <Button href={"https://github.com/HeatherAckerman"} target="blank" className="contactBtn">GitHub</Button>
-      </Row>
-      <br />
-      <Row>
-        <h5>Email:</h5>
-      </Row>
-      <Row>
-        <p>hla20115213@gmail.com</p>
       </Row>
     </Container>
   </div>
