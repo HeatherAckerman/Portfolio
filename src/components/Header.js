8aa05c8920bb94918280a77d4c5191292fd9e04b
HeatherAckerman <hla20115213@gmail.com>
2020-12-20 10:52:54 -0600

    centered and capatilized links in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,11 @@ const Header = () => (
   <>
   <StickyHeader>
     <Navbar variant="dark" style={{backgroundColor: " rgb(0,60,82)", position: "fixed", width: 100 + "%", top: 0, display: "block", padding: 10, zIndex: 2}}>
-      <Nav className="mr-auto">
-        <Nav.Link href="#about" style={{color: "white"}}>About</Nav.Link>
-        <Nav.Link href="#projects" style={{color: "white"}}>Projects</Nav.Link>
-        <Nav.Link href="#skills" style={{color: "white"}}>Skills</Nav.Link>
-        <Nav.Link href="#contact" style={{color: "white"}}>Contact</Nav.Link>
+      <Nav class="row justify-content-center">
+        <Nav.Link href="#about" style={{color: "white"}}>ABOUT</Nav.Link><div style={{color: "white"}}>|</div>
+        <Nav.Link href="#projects" style={{color: "white"}}>PROJECTS</Nav.Link><div style={{color: "white"}}>|</div>
+        <Nav.Link href="#skills" style={{color: "white"}}>SKILLS</Nav.Link><div style={{color: "white"}}>|</div>
+        <Nav.Link href="#contact" style={{color: "white"}}>CONTACT</Nav.Link>
       </Nav>
       </Navbar>
       </StickyHeader>
