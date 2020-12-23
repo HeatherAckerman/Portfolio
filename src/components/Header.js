89a468c9f204167febf8c98e1b45e94ec4831fa7
HeatherAckerman <hla20115213@gmail.com>
2020-12-23 12:13:33 -0600

    reordered the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,9 @@ const Header = () => (
   <StickyHeader>
     <Navbar variant="dark" style={{backgroundColor: " rgb(0,60,82)", position: "fixed", width: 100 + "%", top: 0, display: "block", padding: 10, zIndex: 2}}>
       <Nav class="row justify-content-center">
-        <Nav.Link href="#about" style={{color: "white"}}>ABOUT</Nav.Link><div style={{color: "white"}}>|</div>
         <Nav.Link href="#projects" style={{color: "white"}}>PROJECTS</Nav.Link><div style={{color: "white"}}>|</div>
         <Nav.Link href="#skills" style={{color: "white"}}>SKILLS</Nav.Link><div style={{color: "white"}}>|</div>
+        <Nav.Link href="#about" style={{color: "white"}}>ABOUT</Nav.Link><div style={{color: "white"}}>|</div>
         <Nav.Link href="#contact" style={{color: "white"}}>CONTACT</Nav.Link>
       </Nav>
       </Navbar>
