ca70dfafffb9d14e9b48ccf2543682c8bf4004a8
Heather Ackerman <69919170+HeatherAckerman@users.noreply.github.com>
2021-02-22 19:36:15 -0600

    styled the ContactForm and Contact Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,10 +11,14 @@ function Contact() {
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col">
-                        <a name="contact"><h1>Contact<div>If you have feedback, advice, or would like to collaborate on a project please reach out, I would love to hear from you. </div></h1></a><hr></hr>
+                        <a name="contact">
+                            <h1>Contact</h1><hr/>
+                            <h2>Wanna collaborate on a project? Have feedback? Know of an employment opportunity? I’d love to hear from you! Send me a message using the form or one of the links below.</h2>
+                        </a><hr/>
                     </div>
                 </div>
-                <br />
+                <ContactForm />
+                <hr/>
                 <div class="row justify-content-center">
                     <div class="col">
                         <a href={"https://github.com/HeatherAckerman"} target="blank" ><img src={GitHub} className="contactBtn" /><br />GitHub</a>
@@ -26,7 +30,6 @@ function Contact() {
                         <a href={"mailto:hla20115213@gmail.com"} target="blank" rel="noopener noreferrer"><img src={email} className="contactBtn" /><br />Email</a>
                     </div>
                 </div>
-                <ContactForm/>
             </div>
         </div>
     );
--- src/contact/index.js
+++ src/contact/index.js
@@ -1,70 +1,74 @@
 import React, { useState } from 'react';
 import { Axios, db } from '../firebase/firebaseConfig';
+import '../App.css'
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({})
+    const [formData, setFormData] = useState({})
 
-  const updateInput = e => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    })
-  }
-  const handleSubmit = event => {
-    event.preventDefault()
-    sendEmail()
-    setFormData({
-      name: '',
-      email: '',
-      message: '',
-    })
-  }
-  const sendEmail = () => {
-    Axios.post(
-      'https://us-central1-portfolio-e79bb.cloudfunctions.net/submit',
-      formData
-    )
-      .then(res => {
-        db.collection('emails').add({
-          name: formData.name,
-          email: formData.email,
-          message: formData.message,
-          time: new Date(),
+    const updateInput = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
         })
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
+    }
+    const handleSubmit = event => {
+        event.preventDefault()
+        sendEmail()
+        setFormData({
+            name: '',
+            email: '',
+            message: '',
+        })
+    }
+    const sendEmail = () => {
+        Axios.post(
+            'https://us-central1-portfolio-e79bb.cloudfunctions.net/submit',
+            formData
+        )
+            .then(res => {
+                db.collection('emails').add({
+                    name: formData.name,
+                    email: formData.email,
+                    message: formData.message,
+                    time: new Date(),
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          onChange={updateInput}
-          value={formData.name || ''}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={updateInput}
-          value={formData.email || ''}
-        />
-        <textarea
-          type="text"
-          name="message"
-          placeholder="Message"
-          onChange={updateInput}
-          value={formData.message || ''}
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
-    </>
-  )
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <h3>Send Me a Message</h3>
+                <input
+                    className="inputClass"
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    onChange={updateInput}
+                    value={formData.name || ''}
+                /><br />
+                <input
+                    className="inputClass"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    onChange={updateInput}
+                    value={formData.email || ''}
+                /><br />
+                <textarea
+                    type="text"
+                    name="message"
+                    placeholder="Message"
+                    onChange={updateInput}
+                    value={formData.message || ''}
+                ></textarea><br />
+                <button className="submitBtn" type="submit">Submit</button>
+            </form>
+        </>
+    )
 }
 
 export default ContactForm
